a4befc3a32a505bbf68c9de04450d4da4e9333f1
JulianSuarez5 <Juacosuarez12@gmail.com>
2024-05-27 06:39:25 -0500

    New Changes

--- CapaDatos/clsConexion.cs
+++ CapaDatos/clsConexion.cs
@@ -5,11 +5,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Configuration;
+using System.Security;
 
 namespace CapaDatos
 {
     public class clsConexion
     {
-        public static string Cadena = ConfigurationManager.ConnectionStrings["Cadena_Conexion"].ToString();
+        public static string Cadena = "Data Source=sqlserver1.czo4ooikuy80.us-east-2.rds.amazonaws.com;Initial Catalog=BD_SISTEMA_VENTAS;User ID=Julian;Password=Juaco666";
     }
 }
